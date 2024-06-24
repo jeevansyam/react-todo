fdd0c67802cdaac5d99d8a5d00d32db3d0342709
sparsh-iMac <sparsh-imac@sparsh-iMacs-iMac.local>
2024-06-24 19:08:09 +0530

    update

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,53 +3,78 @@ import './App.css';
 
 function Todo() {
 
-    const [inputVal, setInputVal] = useState('');
-    const [todoList, setTodoList] = useState([]);
-    const [eIndex, setEIndex] = useState();
-    const [eIndexVal, setIndexVal] = useState();
-    
-    const inputHandle = (e) => {
-        setInputVal(e.target.value)
+    const [searchInputVal, setSearchInputVal] = useState('');
+
+   const TodoListObj = [
+    {
+        _id: 1,
+        issueNo: 1124,
+        taskName: 'Dropdown Issue',
+        status: false
+    },
+    {
+        _id: 2,
+        issueNo: 1125,
+        taskName: 'Table Alignment Issue',
+        status: false
+    },
+    {
+        _id: 3,
+        issueNo: 1126,
+        taskName: 'Popup',
+        status: false
+    },
+    {
+        _id: 4,
+        issueNo: 1127,
+        taskName: 'Button Color',
+        status: false
     }
-    const submitHandler = (e, index) => {
-        e.preventDefault();
-        if( inputVal.trim() !== ''){
-            setTodoList([...todoList, inputVal.trim()])
-            todoList.find(fItem => fItem == inputVal.trim());
-            console.log(eIndexVal);
-        }
-        setInputVal('');
-    }
-    const itemDelete = (indexItem) => {
-        setTodoList(todoList.filter((todoItem,item)=> item !== indexItem ));
-        console.log(typeof objTodoList);
-    }
-   const itemEdit = (indexItem, todoItem) => {
-        console.log(indexItem, todoItem);
-        setInputVal(todoItem);
-        setEIndex(indexItem);
-        
+   ];
+   const searchInputHandler = (e) => {
+        const inVal = e.target.value;
+        setSearchInputVal(inVal.trim())
+        const dd = TodoListObj.filter( item => TodoListObj.includes())
+        console.log(dd);
    }
   return (
     <div className="todo-container">
-        {/* <h1>My To-Do List</h1>
-        <form onSubmit={submitHandler} id="todo-form">
-            <input type="text" placeholder="Add a new task..." onChange={inputHandle} value={inputVal} />
-            <button type="submit">Add</button>
-        </form>
-        <ul id="todo-list">
-          {
-            todoList.map( (todoItem, index) => {
-                return(
-                    <li key={index}>
-                        {todoItem}
-                        <span onClick={ ()=> itemEdit(index, todoItem)}>Edit</span>
-                        <span onClick={ ()=> itemDelete(index, todoItem)}>Delete</span>
-                    </li>
-                )
-            })
-          }
-        </ul> */}
+
+        <div className="filterDiv">
+            <input type="text" onChange={searchInputHandler} value={searchInputVal} />
+        </div>
+        <br />
+        <table cellPadding={5} cellSpacing={0} style={{border: '1px solid #ccc', width: '100%'}} >
+            <thead>
+                <tr>
+                    <th>S. No</th>
+                    <th>Issue ID</th>
+                    <th>Task Name</th>
+                    <th>Task Status</th>
+                    <th colSpan={2}>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+               
+            {
+                TodoListObj.map( item => {
+                    return(
+                        <tr key={item._id}>
+                            <td>{item._id}</td>
+                            <td>{item.issueNo}</td>
+                            <td>{item.taskName}</td>
+                            <td>{item.status}</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                        </tr>
+                    )
+                })
+            }
+            </tbody>
+        </table>
+
+
+       
     </div>
   )
 }
